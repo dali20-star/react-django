142e9e682414e6368681c159434e7243cbecae65
ahmed <ahmedmasmoudi@gmail.com>
2025-07-24 02:33:05 +0100

    Add frontend and backend folders

--- backend/backend_project/urls.py
+++ backend/backend_project/urls.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('core.urls')),  # ou testresults selon ton app
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('api/', include('core.urls')),            # API routes
+    path('', include('testresults.urls')),         # App de résultats
+    path('', lambda request: HttpResponse("Hello from Django backend!")),  # Page d’accueil
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
